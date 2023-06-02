e9cfe4b090194ee480461397711d72ff97f50b63
Shoaib Amjad <muhammad.shoaib@crowdbotics.com>
2023-06-03 00:54:37 +0500

    fonts integrations

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "testmodules-41093",
   "url": "https://testmodules-41093.botics.co",
   "api": "https://testmodules-41093.botics.co/api/v1",
-  "initialRoute": "Untitled1"
+  "initialRoute": "Home"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
---
+++ screens/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+
+const Home = () => {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ fontFamily: "OpenSans-Bold", fontSize: 27 }}>Yatra-One</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+export default Home;
